14711755d823440259e8354224333ca54cf3e62c
XOneFG <117354752+VsrStudio@users.noreply.github.com>
2024-11-15 14:33:16 +0700

    final

--- src/VsrStudio/LevelSystem/LevelSystemScore.php
+++ src/VsrStudio/LevelSystem/LevelSystemScore.php
@@ -5,24 +5,34 @@ namespace VsrStudio\LevelSystem;
 use Ifera\ScoreHud\event\PlayerTagUpdateEvent;
 use Ifera\ScoreHud\scoreboard\ScoreTag;
 use pocketmine\player\Player;
+use Ifera\ScoreHud\ScoreHud;
 
-class LevelSystemScore {
+class Main extends PluginBase {
 
-    private $plugin;
+    private $playerData;
 
-    public function __construct(Main $plugin) {
-        $this->plugin = $plugin;
+    public function onEnable(): void {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
     }
 
     public function onPlayerTagUpdate(PlayerTagUpdateEvent $event): void {
         $player = $event->getPlayer();
         $name = $player->getName();
-        $data = $this->plugin->getPlayerData($name);
+        $data = $this->getPlayerData($name);
 
-        $scoreTagLevel = new ScoreTag("{levelsystem.level}", (string)$data["level"]);
-        $scoreTagExp = new ScoreTag("{levelsystem.exp}", (string)$data["exp"]);
+        $levelTag = new ScoreTag("{levelsystem.level}", (string) $data["level"]);
+        $expTag = new ScoreTag("{levelsystem.exp}", (string) $data["exp"]);
 
-        $this->plugin->getServer()->getPluginManager()->getPlugin("ScoreHud")
-            ->updateScoreTags($player, [$scoreTagLevel, $scoreTagExp]);
+        $scoreHud = $this->getServer()->getPluginManager()->getPlugin("ScoreHud");
+
+        if ($scoreHud !== null && $scoreHud->isEnabled()) {
+            $scoreHud->getScoreHudManager()->updatePlayerTags($player, [$levelTag, $expTag]);
+
+            $this->getLogger()->info("Scoreboard pemain {$name} diperbarui.");
+        }
+    }
+
+    public function getPlayerData(string $name): array {
+        return $this->playerData->get($name, ["level" => 1, "exp" => 0]);
     }
 }
